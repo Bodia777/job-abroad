e186bc1b03e86b14fd5b13c24211ffa6fe38b311
Bogdan Kmet <56129358+Bodia777@users.noreply.github.com>
2020-07-17 04:24:29 +0300

    I have added purposses and aboutUs sections

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, HostListener } from '@angular/core';
 import { LanguageService } from 'src/app/services/language.service';
 import * as AOS from 'aos';
+import { Text } from 'src/app/interfaces/language.interface';
 
 @Component({
   selector: 'app-main',
@@ -8,15 +9,24 @@ import * as AOS from 'aos';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  public language = [];
+  public language: Text;
 
 
-  @ViewChild('dropdown', {static: false}) dropdown: ElementRef;
-  @ViewChild('li', {static: false}) dropdownItem: ElementRef;
-  @ViewChild('li2', {static: false}) dropdownItem2: ElementRef;
+  @ViewChild('dropdown', {
+    static: false
+  }) dropdown: ElementRef;
+  @ViewChild('li', {
+    static: false
+  }) dropdownItem: ElementRef;
+  @ViewChild('li2', {
+    static: false
+  }) dropdownItem2: ElementRef;
+  @ViewChild('picture', {
+    static: false
+  }) picture: ElementRef;
 
 
-  constructor(private renderer: Renderer2, private languageService: LanguageService) { }
+  constructor(private renderer: Renderer2, private languageService: LanguageService) {}
 
   ngOnInit(): void {
     AOS.init();
@@ -24,24 +34,36 @@ export class MainComponent implements OnInit {
   }
   public openlngMenu(): void {
     if (this.dropdownItem2.nativeElement.style.display === 'none') {
-    this.renderer.setStyle(this.dropdown.nativeElement, 'height', '40px');
-    this.renderer.setStyle(this.dropdownItem.nativeElement, 'display', 'block');
-    this.renderer.setStyle(this.dropdownItem2.nativeElement, 'display', 'block');
+      this.renderer.setStyle(this.dropdown.nativeElement, 'height', '40px');
+      this.renderer.setStyle(this.dropdownItem.nativeElement, 'display', 'block');
+      this.renderer.setStyle(this.dropdownItem2.nativeElement, 'display', 'block');
     } else {
+      this.renderer.setStyle(this.dropdown.nativeElement, 'height', '0px');
+      this.renderer.setStyle(this.dropdownItem.nativeElement, 'display', 'none');
+      this.renderer.setStyle(this.dropdownItem2.nativeElement, 'display', 'none');
+    }
+  }
+  public chooseLanguage(language): void {
+    if (language === 'ua') {
+      this.language = this.languageService.languageUkrainian;
+    }
+    if (language === 'ru') {
+      this.language = this.languageService.languageRussian;
+    }
     this.renderer.setStyle(this.dropdown.nativeElement, 'height', '0px');
     this.renderer.setStyle(this.dropdownItem.nativeElement, 'display', 'none');
     this.renderer.setStyle(this.dropdownItem2.nativeElement, 'display', 'none');
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  public onScroll($event): void {
+    if (globalThis.scrollY > globalThis.innerHeight) {
+      this.renderer.setStyle(this.picture.nativeElement, 'top', '85px');
+      this.renderer.setAttribute(this.picture.nativeElement, 'src', '/assets/images/workersGroup.png');
+    } else {
+      const pictureTop = 85 - 150 * globalThis.scrollY / globalThis.innerHeight;
+      this.renderer.setStyle(this.picture.nativeElement, 'top', `${pictureTop}px`);
+      this.renderer.setAttribute(this.picture.nativeElement, 'src', '/assets/images/mainCity.jpg');
     }
- }
- public chooseLanguage(language): void {
-  if (language === 'ua') {
-    this.language = this.languageService.languageUkrainian;
   }
-  if (language === 'ru') {
-    this.language = this.languageService.languageRussian;
-  }
-  this.renderer.setStyle(this.dropdown.nativeElement, 'height', '0px');
-  this.renderer.setStyle(this.dropdownItem.nativeElement, 'display', 'none');
-  this.renderer.setStyle(this.dropdownItem2.nativeElement, 'display', 'none');
- }
 }
---
+++ src/app/interfaces/language.interface.ts
@@ -0,0 +1,7 @@
+export class Text {
+    headerText: string[];
+    missionText: string[];
+    purposesText: string[];
+    aboutUsTitle: string[];
+    aboutUs: string[];
+}
