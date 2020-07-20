fc2d449a69df264cff4d7cf06245a398771834ac
Bogdan Kmet <56129358+Bodia777@users.noreply.github.com>
2020-07-20 14:58:00 +0300

    I have added logic for Phone input in modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,11 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Text } from 'src/app/interfaces/language.interface';
-import { LanguageService } from 'src/app/services/language.service';
-import { ServerConnectionService } from 'src/app/services/server-connection.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import {
+  BsModalRef,
+  BsModalService
+} from 'ngx-bootstrap/modal';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
+import {
+  Text
+} from 'src/app/interfaces/language.interface';
+import {
+  LanguageService
+} from 'src/app/services/language.service';
+import {
+  ServerConnectionService
+} from 'src/app/services/server-connection.service';
+import {
+  Subject
+} from 'rxjs';
+import {
+  takeUntil
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-modal',
@@ -24,7 +45,7 @@ export class ModalComponent implements OnInit, OnDestroy {
   constructor(
     private fb: FormBuilder, public modalRef: BsModalRef, private serverConnectionService: ServerConnectionService,
     private modalService: BsModalService, private languageService: LanguageService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
     this.createForm();
@@ -42,16 +63,16 @@ export class ModalComponent implements OnInit, OnDestroy {
   }
 
   public getErrorMessage(formControl, regExp): string | null {
-    if (this.connectionForm.controls[formControl].hasError('required') ||  this.connectionForm.controls[formControl].value.match(/^(\+38-0__-___-__-__)$/)) {
+    if (this.connectionForm.controls[formControl].hasError('required') || this.connectionForm.controls[formControl].value.match(/^(\+38-0__-___-__-__)$/)) {
       return this.content.modalMessage.requiredMessage;
     }
-    if ( this.connectionForm.controls[formControl].dirty || this.connectionForm.controls[formControl].touched) {
-    if (!this.connectionForm.controls[formControl].value.match(regExp)) {
-      this.connectionForm.controls[formControl].setErrors({});
-      return this.content.modalMessage.patternMessage;
+    if (this.connectionForm.controls[formControl].dirty || this.connectionForm.controls[formControl].touched) {
+      if (!this.connectionForm.controls[formControl].value.match(regExp)) {
+        this.connectionForm.controls[formControl].setErrors({});
+        return this.content.modalMessage.patternMessage;
+      }
+      return null;
     }
-    return null;
-  }
   }
 
   public aproveForm(): void {
@@ -64,14 +85,48 @@ export class ModalComponent implements OnInit, OnDestroy {
     this.connectionForm = this.fb.group({
       name: ['', [Validators.required, Validators.pattern]],
       email: ['', [Validators.required, Validators.pattern]],
-      phone: ['+38-0__-___-__-__', [Validators.required, Validators.pattern]],
+      phone: ['+38-0', [Validators.required, Validators.pattern]],
       comment: [''],
     });
   }
 
   private subscLanguage(): void {
     this.languageService.content$
-    .pipe(takeUntil(this.unsubscribed))
-    .subscribe((value: Text) => this.content = value);
+      .pipe(takeUntil(this.unsubscribed))
+      .subscribe((value: Text) => this.content = value);
+  }
+
+  public addRequiredFormatToNumber(event): void {
+    const phoneNumber = this.connectionForm.controls.phone;
+    if (phoneNumber.value.length < 5) {
+      phoneNumber.setValue('+38-0');
+    }
+    if (phoneNumber.value.length >= 5 && phoneNumber.value.slice(0, 5) !== '+38-0') {
+      phoneNumber.setValue(`+38-0${phoneNumber.value.slice(5)}`);
+    }
+    this.replaceNumberWithDash(phoneNumber, event);
+  }
+
+  private replaceNumberWithDash(phoneNumber, event): void {
+    let result = phoneNumber.value.slice(0, 5) + phoneNumber.value.slice(5).split('-').join('');
+    if (result.length > 7) {
+      phoneNumber.setValue(`${result.slice(0, 7)}-${result.slice(7)}`);
+  }
+    result = phoneNumber.value;
+    if (result.length > 11) {
+      phoneNumber.setValue(`${result.slice(0, 11)}-${result.slice(11)}`);
+  }
+    result = phoneNumber.value;
+    if (result.length > 14) {
+      phoneNumber.setValue(`${result.slice(0, 14)}-${result.slice(14)}`);
+  }
+    if (phoneNumber.value.length > 17) {
+      phoneNumber.setValue(`${phoneNumber.value.slice(0, 17)}`);
+    }
+    if (phoneNumber.value.length === 7 || phoneNumber.value.length === 11 || phoneNumber.value.length === 14) {
+      if (event.keyCode !== 8) {
+      phoneNumber.setValue(`${phoneNumber.value}-`);
+    }
+  }
   }
 }
